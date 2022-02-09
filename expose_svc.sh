9ec156f4e2e2c08da018998629c78e0dda07934e
edubonifs <60875825+edubonifs@users.noreply.github.com>
2022-02-09 13:11:40 +0100

    Update expose_svc.sh

--- expose_svc.sh
+++ expose_svc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-oc adm policy add-cluster-role-to-user cluster-monitoring-view -z grafana-serviceaccount -n grafana
+oc adm policy add-cluster-role-to-user cluster-monitoring-view -z grafana-serviceaccount -n monitoring
 oc expose svc/grafana-service -n monitoring
 oc expose svc/pushgateway -n monitoring
